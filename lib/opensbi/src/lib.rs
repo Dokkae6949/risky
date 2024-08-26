2a8e051ff9ed64bc593d5d304c08f0c0f244c59f
Dokkae6949 <finnliry@gmail.com>
2024-08-26 15:48:56 +0200

    Something something

---
+++ lib/opensbi/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+pub mod call;
+pub mod dbcn;
+pub mod hsm;
+
+pub use call::*;
+pub use dbcn::*;
+pub use hsm::*;
+
+/// Returns the address split up into (lo, hi)
+pub fn split_address(address: usize) -> (usize, usize) {
+    if cfg!(target_pointer_width = "64") {
+        (address, 0)
+    } else if cfg!(target_pointer_width = "32") {
+        (address & 0xFFFF_FFFF, address >> 32)
+    } else {
+        unimplemented!()
+    }
+}
---
+++ src/arch/rv64/entry.rs
@@ -0,0 +1,51 @@
+use alloc::boxed::Box;
+use core::fmt::Write;
+use core::arch::{asm, global_asm};
+use core::ops::Deref;
+use opensbi::{hart_get_status, hart_start, HartState};
+use crate::allocator;
+use crate::arch::logger::OpenSbiLogger;
+use crate::arch::rv64::asm::{get_hart_id, init_stack_pointer};
+use crate::logger::LOGGER;
+
+global_asm!(include_str!("asm/boot.S"));
+
+/// Test of zero values in BSS.
+static BSS_TEST_ZERO: usize = 0;
+/// Test of non-zero values in data.
+static DATA_TEST_NONZERO: usize = 0xFFFF_FFFF_FFFF_FFFF;
+
+#[no_mangle]
+pub unsafe extern "C" fn kentry(hart_id: usize) -> ! {
+    assert_eq!(BSS_TEST_ZERO, 0);
+    assert_eq!(DATA_TEST_NONZERO, 0xFFFF_FFFF_FFFF_FFFF);
+
+    println!("Hart {} started", hart_id);
+
+    println!("Initializing logger...");
+    LOGGER.set_logger(Box::new(OpenSbiLogger));
+    println!("Logger initialized");
+
+    println!("Initializing allocator...");
+    allocator::init();
+    println!("Allocator initialized");
+
+    println!("Starting other harts...");
+    for hid in 0..4 {
+        let result = hart_start(hid, kentry_ap as usize, 0);
+        println!("  Starting Hart {}: {:?}", hid, result)
+    }
+
+    crate::kmain();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn kentry_ap() -> ! {
+    init_stack_pointer();
+
+    let hart_id = get_hart_id();
+
+    println!("Hart {} started (AP)", hart_id);
+
+    crate::kmain_ap();
+}
