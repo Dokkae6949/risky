b3bb6c0afb16b959b3a27537e588d9069851bc27
Dokkae6949 <finnliry@gmail.com>
2025-01-25 13:45:13 +0000

    Basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[macro_use]
+mod arch;
+
+#[no_mangle]
+pub extern "C" fn kmain() -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn kmain_ap() -> ! {
+    loop {}
 }
